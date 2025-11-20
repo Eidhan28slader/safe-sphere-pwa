8a1d5af0b80d42b65ed023f98d0b60dc8113cdd7
Eidhan28slader <eidhan.salinas@gmail.com>
2025-11-20 00:28:41 -0500

    Add updateDoc and deleteDoc functions to Firebase

--- firebase.js
+++ firebase.js
@@ -1,14 +1,16 @@
 // Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, serverTimestamp, onSnapshot, query, orderBy } 
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+  getFirestore, collection, addDoc, serverTimestamp, 
+  onSnapshot, query, orderBy, updateDoc, deleteDoc, doc 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Firebase configuration
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDAEvBI81KWzqc-yS-aTmAgRIGaUomfzY0",
+  apiKey: "AIzaSyDAEvBI8IKWzqc-yS-aTmAgRlGaUomfzY0",
   authDomain: "safe-sphere-29f3f.firebaseapp.com",
   projectId: "safe-sphere-29f3f",
-  storageBucket: "safe-sphere-29f3f.firebasestorage.app",
+  storageBucket: "safe-sphere-29f3f.appspot.com",
   messagingSenderId: "250192856440",
   appId: "1:250192856440:web:b6035ce3941955928d348d"
 };
@@ -18,5 +20,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Export
-export { db, collection, addDoc, serverTimestamp, onSnapshot, query, orderBy };
-
+export { db, collection, addDoc, serverTimestamp, onSnapshot, query, orderBy, updateDoc, deleteDoc, doc };
