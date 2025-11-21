efeb9984773ef5c58abd3cea022fcbd84742a581
Eidhan28slader <eidhan.salinas@gmail.com>
2025-11-20 23:50:32 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import {
   onSnapshot, query, orderBy, updateDoc, deleteDoc, doc 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Firebase config
+// 🔴 IMPORTANTE: pega aquí EXACTO lo que te da Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyDAEvBI8IKWzqc-yS-aTmAgRlGaUomfzY0",
+  apiKey: "TU_API_KEY_REAL",
   authDomain: "safe-sphere-29f3f.firebaseapp.com",
   projectId: "safe-sphere-29f3f",
-  storageBucket: "safe-sphere-29f3f.appspot.com",
+  storageBucket: "safe-sphere-29f3f.firebasestorage.app",
   messagingSenderId: "250192856440",
   appId: "1:250192856440:web:b6035ce3941955928d348d"
 };
