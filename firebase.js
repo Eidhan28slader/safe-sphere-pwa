8043e4790d6bbf51933b307180ecf0dc5ad25200
Eidhan28slader <eidhan.salinas@gmail.com>
2025-11-21 00:52:31 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,37 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "TU_API_KEY",
     authDomain: "safe-sphere-29f3f.firebaseapp.com",
     projectId: "safe-sphere-29f3f",
     storageBucket: "safe-sphere-29f3f.appspot.com",
-    messagingSenderId: "250192856440",
-    appId: "1:250192856440:web:b6035ce394195928d348d"
+    messagingSenderId: "2520192856440",
+    appId: "1:2520192856440:web:b6035ce3941955928d348d"
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
-const auth = getAuth(app);   // 👈 ESTA ES LA INSTANCIA CORRECTA
 
-export { db, auth, collection, addDoc, serverTimestamp };
-
-
+export { 
+    auth,
+    db,
+    collection,
+    addDoc,
+    serverTimestamp,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged
+};
