f1454c3652918ba83b6db33106060e62ee1b451a
Eidhan28slader <eidhan.salinas@gmail.com>
2025-11-21 00:00:36 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,18 @@
-// Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-  getFirestore, collection, addDoc, serverTimestamp, 
-  onSnapshot, query, orderBy, updateDoc, deleteDoc, doc 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-firestore.js";
 
-// 🔴 IMPORTANTE: pega aquí EXACTO lo que te da Firebase Console
 const firebaseConfig = {
   apiKey: "TU_API_KEY_REAL",
   authDomain: "safe-sphere-29f3f.firebaseapp.com",
   projectId: "safe-sphere-29f3f",
-  storageBucket: "safe-sphere-29f3f.firebasestorage.app",
+  storageBucket: "safe-sphere-29f3f.appspot.com",
   messagingSenderId: "250192856440",
   appId: "1:250192856440:web:b6035ce3941955928d348d"
 };
 
-// Initialize
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Export
-export { db, collection, addDoc, serverTimestamp, onSnapshot, query, orderBy, updateDoc, deleteDoc, doc };
+export { db, collection, addDoc, serverTimestamp };
+
+
