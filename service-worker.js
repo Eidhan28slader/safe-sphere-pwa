0bb56081b2a0925b4bf82b1e647ee950f882dafa
Eidhan28slader <eidhan.salinas@gmail.com>
2025-11-21 10:39:31 -0500

    Refactor service worker install and activate events

--- service-worker.js
+++ service-worker.js
@@ -7,17 +7,12 @@ const URLS_TO_CACHE = [
   './manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
 });
+
 
