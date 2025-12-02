47eeba6fcdb90d4860c90aba1bb9f85a2c801f6d
Eidhan28slader <eidhan.salinas@gmail.com>
2025-12-01 22:44:36 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,16 @@
+const CACHE_NAME = 'safe-sphere-cache-v3';
 
-const CACHE_NAME = 'safe-sphere-cache-v1';
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './styles.css',
-  './manifest.json'
-];
-
-self.addEventListener("install", () => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
 });
-
-
